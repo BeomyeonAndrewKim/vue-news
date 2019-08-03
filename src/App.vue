8186618a8d31bfd8b14106da900a0eaae0afbdb5
BeomyeonAndrewKim <beom911@gmail.com>
2019-08-03 23:52:38 +0900

    feat: add Spinner component

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,38 @@
     <transition name="page">
       <router-view></router-view>
     </transition>
+    <spinner :loading="loadingStatus"></spinner>
   </div>
 </template>
 
 <script>
-import { ToolBar } from "./components";
+import { ToolBar, Spinner } from "./components";
+import bus from "./utils/bus.js";
 export default {
   components: {
-    ToolBar
+    ToolBar,
+    Spinner
+  },
+  data() {
+    return {
+      loadingStatus: false
+    };
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    }
+  },
+  created() {
+    bus.$on("start:spinner", this.startSpinner);
+    bus.$on("end:spinner", this.endSpinner);
+  },
+  beforeDestroy() {
+    bus.$off("start:spinner", this.startSpinner);
+    bus.$off("end:spinner", this.endSpinner);
   }
 };
 </script>
